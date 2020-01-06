2c9e7bd5794aba9a9f7e3a0ab9343cae3631b70b
bryan lurer <blurer@pm.me>
2020-01-06 20:19:02 +0800

    fixed info

--- info.sh
+++ info.sh
@@ -1,15 +1,12 @@
 #!/bin/bash
 
 host="$(hostname)"
-intIP="$(hostname -I)"
 extIP="$(curl -s ifconfig.me)"
-weather="$(curl -s wttr.in/Tampa?format=3)"
-
+weather="$(curl -s wttr.in/?format=3)"
 
 # Sys Info
 echo "Username: ${USER}"
 echo "Hostname: ${host}"
-echo "Internal IP: ${intIP}"
 echo "External IP: ${extIP}"
 echo "Weather: ${weather}"
-
+echo "Load:  $(cat /proc/loadavg)"
